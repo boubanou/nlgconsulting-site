4689a1da1e092899e2f7521820f48f8ba308c730
Greg Block Tech <brenig.g@gmail.com>
2026-09-26 10:18:55 +0300

    SEO: remove static commercial hub links before React mounts

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 
 // Static SEO content is present in the initial HTML for crawlers and removed before React mounts.
 document.querySelector('[data-seo-prerender="true"]')?.remove();
+document.querySelector('[data-commercial-insights="true"]')?.remove();
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
